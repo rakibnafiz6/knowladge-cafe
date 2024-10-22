9bd93463dbd485e181dd653a4a0021836613683e
nafiz rakib <rakibnafiz6@gmail.com>
2024-10-22 09:06:49 -0700

    display load data and add bookmarks list

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
-import profile from '../../assets/profile.jpg'
+import profile from '../../assets/profile.png'
 const Header = () => {
     return (
-        <header className='flex items-center justify-between p-4 mx-4 border-b-2'>
+        <header className='flex items-center justify-between p-4 max-w-6xl mx-auto border-b-2'>
             <h1 className='text-4xl font-bold'>Knowladge Cafe</h1>
-            <img className='w-10 h-10 object-cover rounded-full' src={profile} alt="" />
+            <img className='' src={profile} alt="" />
         </header>
     );
 };
