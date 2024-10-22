20cd2a266866793adac413d3c1cc22180edf21d6
nafiz rakib <rakibnafiz6@gmail.com>
2024-10-22 00:06:54 -0700

    set layout and stop proptypes warning

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
+import profile from '../../assets/profile.jpg'
 const Header = () => {
     return (
-        <div>
+        <header className='flex items-center justify-between p-4 mx-4 border-b-2'>
             <h1 className='text-4xl font-bold'>Knowladge Cafe</h1>
-        </div>
+            <img className='w-10 h-10 object-cover rounded-full' src={profile} alt="" />
+        </header>
     );
 };
 
